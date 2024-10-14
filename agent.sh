0b5ad5defb9953640c18dae5b106fce277ab43e0
Cybrly <19822901+cybrly@users.noreply.github.com>
2024-10-14 04:03:34 -0400

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -1,37 +1,56 @@
 #!/bin/bash
 
+# Function to safely get command output
+safe_get() {
+    output=$($@ 2>/dev/null)
+    if [ $? -ne 0 ]; then
+        echo "N/A"
+    else
+        echo "$output"
+    fi
+}
+
 # Collect system information
-hostname=$(hostname)
-kernel_version=$(uname -r)
-uptime=$(uptime -p)
-cpu_model=$(lscpu | grep "Model name" | sed 's/Model name: *//g' | sort -u | paste -sd '/' -)
-cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
-ram_total=$(free -h | awk '/Mem:/ {print $2}')
-ram_usage=$(free -m | awk '/Mem:/ {printf "%.2f%%", $3*100/$2}')
-disk_total=$(df -h / | awk '/\// {print $2}')
-disk_usage=$(df -h / | awk '/\// {print $5}')
-last_ssh=$(last -1 -R | grep -v 'reboot' | awk '{print $4, $5, $6, $7}' | xargs)
-load_average=$(uptime | awk -F'load average:' '{ print $2 }' | sed 's/,//g')
-logged_in_users=$(who | wc -l)
-open_ports=$(ss -tuln | awk 'NR>1 {print $5}' | cut -d':' -f2 | sort -nu | paste -sd ',' -)
-docker_containers=$(docker ps -q | wc -l 2>/dev/null || echo "N/A")
-updates_available=$(apt list --upgradable 2>/dev/null | grep -c upgradable || echo "N/A")
-os_version=$(lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | head -n1 || uname -om)
+hostname=$(safe_get hostname)
+kernel_version=$(safe_get uname -r)
+uptime=$(safe_get uptime -p)
+cpu_model=$(safe_get lscpu | grep "Model name" | sed 's/Model name: *//g' | sort -u | paste -sd '/' -)
+cpu_usage=$(safe_get top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
+ram_total=$(safe_get free -h | awk '/Mem:/ {print $2}')
+ram_usage=$(safe_get free -m | awk '/Mem:/ {printf "%.2f%%", $3*100/$2}')
+disk_total=$(safe_get df -h / | awk '/\// {print $2}')
+disk_usage=$(safe_get df -h / | awk '/\// {print $5}')
+last_ssh=$(safe_get last -1 -R | grep -v 'reboot' | awk '{print $4, $5, $6, $7}' | xargs)
+load_average=$(safe_get uptime | awk -F'load average:' '{ print $2 }' | sed 's/,//g')
+logged_in_users=$(safe_get who | wc -l)
+open_ports=$(safe_get ss -tuln | awk 'NR>1 {print $5}' | cut -d':' -f2 | sort -nu | paste -sd ',' -)
+docker_containers=$(safe_get docker ps -q | wc -l)
+updates_available=$(safe_get apt list --upgradable 2>/dev/null | grep -c upgradable)
+os_version=$(safe_get lsb_release -ds || cat /etc/*release 2>/dev/null | head -n1 || uname -om)
 
 # Get local IP address
-local_ip=$(hostname -I | awk '{print $1}')
+local_ip=$(safe_get hostname -I | awk '{print $1}')
 
 # Get internal domain
-internal_domain=$(hostname -f)
+internal_domain=$(safe_get hostname -f)
 
 # Fetch IP and location information from ipinfo.io
-ipinfo=$(curl -s ipinfo.io)
-public_ip=$(echo $ipinfo | jq -r '.ip')
-city=$(echo $ipinfo | jq -r '.city')
-region=$(echo $ipinfo | jq -r '.region')
-country=$(echo $ipinfo | jq -r '.country')
-loc=$(echo $ipinfo | jq -r '.loc')
-org=$(echo $ipinfo | jq -r '.org')
+ipinfo=$(curl -s --max-time 5 ipinfo.io)
+if [ $? -eq 0 ]; then
+    public_ip=$(echo $ipinfo | jq -r '.ip // "N/A"')
+    city=$(echo $ipinfo | jq -r '.city // "N/A"')
+    region=$(echo $ipinfo | jq -r '.region // "N/A"')
+    country=$(echo $ipinfo | jq -r '.country // "N/A"')
+    loc=$(echo $ipinfo | jq -r '.loc // "N/A"')
+    org=$(echo $ipinfo | jq -r '.org // "N/A"')
+else
+    public_ip="N/A"
+    city="N/A"
+    region="N/A"
+    country="N/A"
+    loc="N/A"
+    org="N/A"
+fi
 
 # Output as JSON using jq for proper formatting
 jq -n \
